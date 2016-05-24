c25f095d3a6d65e78273aebc72cb1afb7629e66d
tamizhgeek <tamizhgeek@gmail.com>
2016-05-24 11:51:58 +0000

    updated site

--- latest/api/index.js
+++ latest/api/index.js
@@ -1,1 +1,1 @@
-Index.PACKAGES = {"com" : [], "com.indix" : [], "com.indix.utils" : [], "com.indix.utils.core" : [{"object" : "com\/indix\/utils\/core\/UrlUtils$.html", "name" : "com.indix.utils.core.UrlUtils"}]};
+Index.PACKAGES = {"com" : [], "com.indix" : [], "com.indix.utils" : [], "com.indix.utils.core" : [{"object" : "com\/indix\/utils\/core\/UPC$.html", "case class" : "com\/indix\/utils\/core\/UPC.html", "name" : "com.indix.utils.core.UPC"}, {"object" : "com\/indix\/utils\/core\/UrlUtils$.html", "name" : "com.indix.utils.core.UrlUtils"}]};
